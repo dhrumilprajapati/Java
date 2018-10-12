7055cc318bce092c1d180d1dbf274c8cc907ba04
dhrumilprajapati <dhrumil.ap86@gmail.com>
2018-10-12 20:26:46 +0530

    Create 51ModDemo.java
    Demo of Modulo Operator.

---
+++ 51ModDemo.java
@@ -0,0 +1,23 @@
+//Demostrate the % operator.
+    class ModDemo{
+       public static void main(String[] args) {
+       int iresult, irem;
+       double= dresult,drem;
+       
+       iresult=10/3;
+       irem=10%3;
+       
+       dresult=10.0/3.0;
+       
+       System.out.println("Result and remainder of 10/3:" +iresult + " " +irem);
+        System.out.println("Result and remainder of 10.0/3.0:" + dresult + " " +drem);
+      }
+     }
+     
+     /* The Output of the program
+      Result and remainder of 10/3: 3 1
+      Result and remainder of 10.0/3.0: 3.333333333333 1.0
+      */
+     
+       
+       
